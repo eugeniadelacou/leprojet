e9a52588da6e54a02ea1ded9db8282b33cbd47ec
eugeniadelacou <eugenia.delacou@gmail.com>
2018-11-04 09:22:55 +0000

    fini header, debut data

--- ouvrirfichier.c
+++ ouvrirfichier.c
@@ -21,6 +21,8 @@ int lirefichier(char* filename)
   char* flag = 0;
   char* flag2 = 0;
   char* size = 0;
+  //char size_copie[150];
+  int size_val = 0;
 
   fichier = fopen(filename, "r");
 
@@ -35,24 +37,35 @@ int lirefichier(char* filename)
     {
       flag = fgets(ligneactuelle, 50, fichier);
 
-      // fgets stocke ce qu'il lit dans ligneactuelle et renvoi un code pour
-      // dire s'il a réussi à lire ou s'il a renconté une erreur (EOF par ex)
+      /* fgets stocke ce qu'il lit dans ligneactuelle et renvoie un code (flag)
+      pour dire s'il a réussi à lire ou s'il a renconté une erreur (EOF par ex)
+      */
 
       if (!strcmp (ligneactuelle, "[Header Section]\n"))
       {
-      printf("AYAYAYAYA\n");
-      /*strcmp compare les caracteres un a un et retourne le nombre de
+      /* strcmp compare les caracteres un a un et retourne le nombre de
       caracteres differents. Ici on en veut 0 differents. Donc on veut
       "non strcmp", car 0=faux et tout le reste = vrai */
 
-            do {
-              if(fgets(ligneactuelle, 50, fichier)!= NULL)
+            do
+            {
+              if (fgets(ligneactuelle, 50, fichier)!= NULL)
+
+              // on est dans Header. on regarde si ligneactuelle est vide
+
               {
                 strcpy(ligneactuelle_copie, ligneactuelle);
+                /* on fait un clone de lignactuelle que l'on modifiera avec
+                strtok */
+
                 size = strtok(ligneactuelle_copie, "=");
-                
-                size = strtok(NULL, "=");
-                printf("%s", size);
+                if (!strcmp (size, "ImageResolution"))
+                {
+                  size = strtok(NULL, "=x");
+                  printf("%s\n", size);
+                  size_val = atoi(size);
+                  printf("\nla valeur de la taille est %d !\n", size_val);
+                }
               }
               else
               {
@@ -60,11 +73,19 @@ int lirefichier(char* filename)
                   break;
               }
             } while(strcmp (ligneactuelle, "\n"));
+
+            // on s'arrete de lire quand on arrive a un saut de ligne
+
       }
-
-
-       // on affiche size qui correspond a la taille
-
+      if (!strcmp (ligneactuelle, "[Data Section]\n"))
+      {
+      printf("Bienvenue dans le monde de la Data Section\n");
+      }
+      else
+      {
+          printf("erreur en lisant la partie Data\n");
+          break;
+      }
     }
 
       while (flag != NULL); // on continue tant que fgets n'a pas retourné EOF
