341d7d5dec57fd506dd85ad7ed63810a3bfc715b
eugeniadelacou <eugenia.delacou@gmail.com>
2018-11-20 16:51:58 +0000

    la fonction extract_size devient extract_data, les distances entre pixels sont prises en compte

--- projet.c
+++ projet.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-int size_val = 0;
+int image_resolution = 0;
+double espace_pixels = 0;
 
 double** creer_matrice_2D(double** matrice, int x, int y)
 {
@@ -18,17 +19,19 @@ double** creer_matrice_2D(double** matrice, int x, int y)
 return matrice;
 }
 
-int extract_size(FILE* fichier)
+int extract_data(FILE* fichier)
 /* cette fonction récupère dans le fichier texte la taille nécessaire pour la
-matrice */
+matrice, càd le nombre de pixels (image_resolution), et la taille de l'image
+(image_size) */
 {
   char ligneactuelle[150];
   char ligneactuelle_copie[150];
   char* size = 0;
+  int image_size = 0;
 
   do
-  /* on s'arrête à la fin de la ligne (Image Resolution) qui  est codée par
-  \r\n */
+  /* on s'arrête à la fin de la dernière ligne de Data Section, qui  est codée
+  par \r\n */
   {
     if (fgets(ligneactuelle, 50, fichier)!= NULL)
     {
@@ -37,34 +40,44 @@ matrice */
       /* on fait un clone de lignactuelle que l'on modifiera avec strtok */
       size = strtok(ligneactuelle_copie, "=");
       /* strtok garde ce qu'il y a à gauche de la délimitation, ici = */
-      if (!strcmp (size, "Image Resolution"))
-      /* strcmp compare des chaînes de caractères et retourne 0 si elles sont
-      identiques. on veut donc "non strcmp" puisque "false" = 0 */
+      if (!strcmp (size, "Image Size"))
+      {
+        /* strcmp compare des chaînes de caractères et retourne 0 si elles sont
+        identiques. on veut donc "non strcmp" puisque "false" = 0 */
+        size = strtok(NULL, " ");
+        /* en faisant strtok avec NULL, on garde cette fois ce qu'il y a à
+        droite de la délimitation, x */
+        image_size = atoi(size);
+        printf("image_size = %d\n", image_size);
+      }
+      else if (!strcmp (size, "Image Resolution"))
       {
         size = strtok(NULL, "x");
         /* en faisant strtok avec NULL, on garde cette fois ce qu'il y a à
         droite de la délimitation, x */
-        size_val = atoi(size);
+        image_resolution = atoi(size);
       }
     }
   }
   while(strcmp (ligneactuelle, "\r\n"));
+  espace_pixels = (double) image_size/image_resolution;
+  printf("espace_pixels = %f\n", espace_pixels);
   return 0;
 }
 
 double** remplir_matrice(FILE* fichier, double** matrice)
 {
-  int step = 15*size_val;
+  int step = 15*image_resolution;
   /* on va lire combien de caractères ? si chaque pixel fait 11 caractères
-  environ, on prend 15*size_val (taille de la matrice) pour être sûr */
+  environ, on prend 15*image_resolution (taille de la matrice) pour être sûr */
   char ligneactuelle_data[step];
   char ligneactuelle_copie_data[step];
   char* pixel = 0;
   double pixel_val = 0;
 
   fgets(ligneactuelle_data, step, fichier);
-  //  for (int i = 0; i < size_val; i++)
-  for (int i = 0; i < 2; i++)
+  //  for (int i = 0; i < image_resolution; i++)
+  for (int i = 0; i < image_resolution; i++)
   /* boucle de lecture de chaque ligne de pixels */
   {
     fgets(ligneactuelle_data, step, fichier);
@@ -78,9 +91,9 @@ double** remplir_matrice(FILE* fichier, double** matrice)
     pixel_val = atof(pixel);
     matrice[i][0] = pixel_val;
   //  printf("matrice [%d][0] = %f\n", i, pixel_val);
-    for (int j = 1; j < 2; j++)
+    for (int j = 1; j < image_resolution; j++)
     /* boucle de lecture de chaque colonne : attention j commence à 1 */
-//    for (int j = 1; j < size_val; j++)
+//    for (int j = 1; j < image_resolution; j++)
     {
       pixel = strtok(NULL, "\t");
       pixel_val = atof(pixel);
@@ -116,17 +129,17 @@ double** lirefichier(char* filename)
   if(fichier != NULL)
   {
     // le fichier n'est pas vide --> on peut le lire
-    do
-    // on continue tant que fgets n'a pas retourné EOF (flag = NULL)
-    {
-      flag = fgets(ligneactuelle, 50, fichier);
+    flag = fgets(ligneactuelle, 50, fichier);
     /* fgets stocke ce qu'il lit dans ligneactuelle et renvoie un code (flag)
     pour dire s'il a réussi à lire ou s'il a renconté une erreur (EOF par ex) */
+    do
+    {
+      // on continue tant que fgets n'a pas retourné EOF (flag = NULL)
 
       if (!strcmp (ligneactuelle, "[Header Section]\r\n"))
       {
-        extract_size(fichier);
-        printf("\nla valeur de la taille est %d !\n", size_val);
+        extract_data(fichier);
+        printf("\nla valeur de la taille est %d !\n", image_resolution);
       }
     /*  else
       {
@@ -139,9 +152,11 @@ double** lirefichier(char* filename)
         printf("Bienvenue dans le monde de la Data Section\n");
         printf("on essaye de créer la matrice\n");
 
-        matrice = creer_matrice_2D(matrice, size_val, size_val);
+        matrice = creer_matrice_2D(matrice, image_resolution, image_resolution);
         matrice = remplir_matrice(fichier, matrice);
       }
+      flag = fgets(ligneactuelle, 50, fichier);
+
     }
     while (flag != NULL);
     fclose(fichier);
@@ -161,14 +176,15 @@ int creer_fichier_stl(char* fichierstl, double** matrice)
 {
   FILE* fichier = NULL;
   fichier = fopen(fichierstl, "w");
+
   if (fichier != NULL)
   {
     fputs("solid lasurface\n", fichier);
-    for (int i = 0; i < size_val-1; i++)
+    for (int i = 0; i < image_resolution-1; i++)
     /* on risque d'avoir des problèmes avec les dernières lignes et colonnes,
     donc pour éviter ça, on va s'arrêter à une case avant la fin de matrice */
     {
-      for (int j = 0; j < size_val-1; j++)
+      for (int j = 0; j < image_resolution-1; j++)
       {
         fputs("facet normal 0 1 0\n", fichier);
 
@@ -176,18 +192,18 @@ int creer_fichier_stl(char* fichierstl, double** matrice)
         normale, mais on verra ça plus tard */
 
         fputs("\touter loop\n", fichier);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i, j, matrice[i][j]);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i+1, j, matrice[i+1][j]);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i, j+1, matrice[i][j+1]);
-        printf("X = %d %d z %f\n", i, j, matrice[i][j]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", i*espace_pixels, j*espace_pixels, matrice[i][j]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", (i+1)*espace_pixels, j*espace_pixels, matrice[i+1][j]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", i*espace_pixels, (j+1)*espace_pixels, matrice[i][j+1]);
+        //printf("X = %d %d z %f\n", i, j, matrice[i][j]);
         fputs("\tendloop\n", fichier);
         fputs("endfacet\n", fichier);
         fputs("facet normal 0 1 0\n", fichier);
         fputs("\touter loop\n", fichier);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i+1, j, matrice[i+1][j]);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i, j+1, matrice[i][j+1]);
-        fprintf(fichier, "\t \tvertex %d %d %f\n", i+1, j+1, matrice[i+1][j+1]);
-        printf("X = %d %d z %f\n", i, j, matrice[i][j]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", (i+1)*espace_pixels, j*espace_pixels, matrice[i+1][j]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", i*espace_pixels, (j+1)*espace_pixels, matrice[i][j+1]);
+        fprintf(fichier, "\t \tvertex %f %f %f\n", (i+1)*espace_pixels, (j+1)*espace_pixels, matrice[i+1][j+1]);
+        //printf("X = %d %d z %f\n", i, j, matrice[i][j]);
         fputs("\tendloop\n", fichier);
         fputs("endfacet\n", fichier);
 
